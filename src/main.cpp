27d9421cb1c862274bebe1a63de25d21b4694279
HuyHieu <huyhieu.hust2855@gmail.com>
2024-02-21 00:07:48 +0700

    FIX board 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/*
+ND-DA: D1, (đỏ nguôn, cam tín hiệu)
+Mưa: A0 (tím tín hiệu, nguồn xanh lá)
+Relay: D3
+*/
+
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
@@ -10,8 +16,6 @@
 #include <SoftwareSerial.h>
 
 // Wi-Fi information
-// const char *ssid = "BKSTAR";
-// const char *password = "stemstar";
 const char *ssid = "FPT Telecom.2.4G";
 const char *password = "22121999";
 
@@ -25,34 +29,27 @@ const char *mqttPassword = "your_MQTT_password";
 // Thông tin Topic
 // --- Topic: SystemUrl/idgate/type/name
 const char *systemUrl = "3c531531-d5f5-4fe3-9954-5afd76ff2151";
-const char *moduleUrl = "3A1D904C-FA20-44AB-833C-08DC14DB6FCE";
+const char *moduleUrl = "1642BCDF-FEDC-4706-820A-20AA0AA4DF6C";
 
 // const char *ND = "6EA49BEF-B141-4567-B43A-CE4FBF1AD348";
 // const char *DA = "CBBB90A0-14B2-47D1-9EE1-10934185B8AA";
 // const char *MUA = "6239FA8A-129B-46C1-A3CB-328AC318EA07";
 // const char *AD = "0AE1FF7F-450A-4A90-853E-3ED64F2899A4";
 
-const char *ND = "6ea49bef-b141-4567-b43a-ce4fbf1ad348";
-const char *DA = "cbbb90a0-14b2-47d1-9ee1-10934185b8aa";
-const char *MUA = "6239fa8a-129b-46c1-a3cb-328ac318ea07";
-const char *AD = "0ae1ff7f-450a-4a90-853e-3ed64f2899a4";
+const char *ND = "65bbe9da-7163-4689-b20b-9e21dabd4860";
+const char *DA = "720b8be4-7064-43b9-8c6a-106f0accbfaa";
+const char *MUA = "ddca1d26-577d-433c-bcf9-03c83cb18ac1";
 
-const char *QUAT = "B66F2370-D5C4-4A61-84ED-DB9CEBE64E9D";
-const char *MAYBOM = "A61D10C0-B931-4906-98F4-18F8FEF95EB1";
-const char *DEN = "EA5D5EA9-C6F2-4692-AFC3-0344C8638D9E";
+const char *DEN = "ECE53418-503E-4CD3-9BA5-96A0FFFE65B6";
 
 const char *idD8 = "A529949C-252D-42A7-B7EA-4359DFC492B3";
 const char *Async = "async";
 
 // GPIO define gate name
-const uint8_t gateControl1 = D6;
-const uint8_t gateControl2 = D7;
-const uint8_t gateControl3 = D8;
 
-const uint8_t gateMeasure1 = D1; // mua
-const uint8_t gateMeasure2 = D2; // nhiet do do am
-const uint8_t gateMeasure3 = D3;
-const uint8_t gateMeasure4 = D4;
+const uint8_t gateMeasure1 = D1; // nhiet do do am
+
+const uint8_t gateControl1 = D3;
 
 // Initialize DHT to measure temperature and humidity
 DHT dht1(gateMeasure1, DHT11);
@@ -93,20 +90,16 @@ void readControl()
 {
   // Read the status of the GPIOs
   int gateControl1Status = digitalRead(gateControl1);
-  int gateControl2Status = digitalRead(gateControl2);
   // Print
   Serial.print("GPIO 6 :");
   Serial.println(gateControl1Status);
   Serial.print("GPIO 7 :");
-  Serial.println(gateControl2Status);
   String payloadControl1 = String(gateControl1Status);
-  String payloadControl2 = String(gateControl2Status);
   std::string topicControl1 = std::string(systemUrl) + "/w/" + std::string("") + "/res";
   std::string topicControl2 = std::string(systemUrl) + "/w/" + std::string("") + "/res";
 
   // Send to broker
   client.publish(topicControl1.c_str(), payloadControl1.c_str());
-  client.publish(topicControl2.c_str(), payloadControl2.c_str());
 }
 
 // reading status gates measure
@@ -126,15 +119,12 @@ void readDHT11()
   DynamicJsonDocument RES2(capacity);
   RES1[ND] = String(t1);
   RES1[DA] = String(h1);
-  RES2[MUA] = String(mua);
+  RES1[MUA] = String(mua);
 
   String jsonString1;
-  String jsonString2;
   serializeJson(RES1, jsonString1);
-  serializeJson(RES2, jsonString2);
   std::string topic = std::string(systemUrl) + "/r/" + std::string(moduleUrl);
   client.publish((topic).c_str(), jsonString1.c_str());
-  client.publish((topic).c_str(), jsonString2.c_str());
 
   Serial.print("Published temperature: ");
   Serial.println(String(t1));
@@ -171,47 +161,17 @@ void ControlDevice(uint8_t gateControl, int mode)
 when receiving control from the topic and control payload*/
 void controlDeviceByTopic(String topicString, String payload)
 {
-  if (topicString == String(MAYBOM))
-  {
-    if (payload == "1")
-    {
-      ControlDevice(gateControl1, HIGH);
-      Serial.print("Open 0");
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/A61D10C0-B931-4906-98F4-18F8FEF95EB1/c", "c");
-    }
-    else if (payload == "0")
-    {
-      ControlDevice(gateControl1, LOW);
-      Serial.print("Close 0");
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/A61D10C0-B931-4906-98F4-18F8FEF95EB1/c", "c");
-    }
-  }
-  if (topicString == String(QUAT))
-  {
-    if (payload == "1")
-    {
-      digitalWrite(gateControl2, HIGH);
-      Serial.print("Open 1");
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/55E05E4F-575C-4656-8099-50ABCFEE7DE8/c", "c");
-    }
-    else if (payload == "0")
-    {
-      ControlDevice(gateControl2, LOW);
-      Serial.print("Close 1");
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/55E05E4F-575C-4656-8099-50ABCFEE7DE8/c", "c");
-    }
-  }
   if (topicString == String(DEN))
   {
     if (payload == "1")
     {
-      digitalWrite(gateControl3, HIGH);
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/A529949C-252D-42A7-B7EA-4359DFC492B3/c", "c");
+      digitalWrite(gateControl1, HIGH);
+      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/ECE53418-503E-4CD3-9BA5-96A0FFFE65B6/c", "c");
     }
     else if (payload == "0")
     {
-      ControlDevice(gateControl3, LOW);
-      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/A529949C-252D-42A7-B7EA-4359DFC492B3/c", "c");
+      ControlDevice(gateControl1, LOW);
+      client.publish("3c531531-d5f5-4fe3-9954-5afd76ff2151/w/ECE53418-503E-4CD3-9BA5-96A0FFFE65B6/c", "c");
     }
   }
 }
@@ -283,15 +243,8 @@ void setup()
   }
 
   pinMode(gateControl1, OUTPUT);
-  pinMode(gateControl2, OUTPUT);
-  pinMode(gateControl3, OUTPUT);
   pinMode(gateMeasure1, INPUT);
-  pinMode(gateMeasure2, INPUT);
-  pinMode(gateMeasure3, INPUT);
-  pinMode(gateMeasure4, INPUT);
-
   digitalWrite(gateControl1, LOW);
-  digitalWrite(gateControl2, LOW);
 
   dht1.begin();
 }
